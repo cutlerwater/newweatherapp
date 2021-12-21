85f902ebc8b1d058595ae8ef15c62a28dbe744db
cutlerwater <cutlerwater2@live.com>
2021-12-21 15:37:43 -0500

    changing to farenheit

--- src/api/FetchForecast.js
+++ src/api/FetchForecast.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const URL = 'https://api.openweathermap.org/data/2.5/forecast'
 const API_KEY = '99b0fa765a69852257a6bad630c1dff9';
 
-export const fetchWeather = async(query) => {
+export const fetchForecast = async(query) => {
     const {data} = await axios.get(URL,{
         params:{
             q : query,
-            units: 'imperial',
+            units: 'units',
             
             APPID: API_KEY
         }
